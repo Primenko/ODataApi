b5807fe0cd35f4f3184a5003c018ab1827f913d5
Primenko Sergey <primenko.segey.i@gmail.com>
2019-07-22 16:09:25 +0300

    master

--- ODataApi.php
+++ ODataApi.php
@@ -24,6 +24,11 @@ class ODataApi
      */
     private $header;
 
+    /**
+     * @var string
+     */
+    private $urlParams;
+
     // using in API OData
     public $filter;
     public $orderby;
@@ -38,25 +43,50 @@ class ODataApi
         $this->url = $url;
     }
 
+    /**
+     * @return string
+     */
     public function getHash()
     {
         return $this->hash;
     }
 
+    /**
+     * @return string
+     */
     public function getUrl()
     {
         return $this->url;
     }
 
+    /**
+     * @param $header
+     */
     public function setHeader($header)
     {
         $this->header = $header;
     }
 
+    /**
+     * @param $urlParams
+     */
+    public function setUrlParams($urlParams)
+    {
+        $this->urlParams = $urlParams;
+    }
+
+    /**
+     * @return bool|string
+     */
     public function query()
     {
+        $url = $this->url;
         $ch = curl_init();
-        curl_setopt($ch, CURLOPT_URL, $this->url);
+
+        if (!empty($this->urlParams))
+            $url .= $this->urlParams;
+
+        curl_setopt($ch, CURLOPT_URL, $url);
 
         if (!empty($this->header))
             curl_setopt($ch, CURLOPT_HTTPHEADER, $this->header);
